8346498da40fe8fd755398119415d8e7069a28cd
Saif04 <36266647+Saif04@users.noreply.github.com>
2019-01-30 18:50:03 +1000

    Add files via upload
    edit product and edit products

--- editproduct.php
+++ editproduct.php
@@ -5,75 +5,25 @@ session_start();
 include_once('include/config.php');
 ?>
 <html lang="en">
-
-	<head>
-  <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Golden Lor</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-  <link href="newstyles.css" rel="stylesheet" type="text/css">
-     
-
-   <nav class="navbar navbar-expand-md navbar-light bg-light bg-light sticky-top">
-      <!--- md for breakpoint for pixel to expanded full site navigation , nav light color and background light sticky to fix navigation-->
-  <div class="conainer-fluid">
-    <!-- container fluid for 100% of screen-->
-    <a class="navbar-brand" href=""><img src="img/logo-full.png" class="img-fluid"></a> 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-      <!--  toggle switch for navigation , collapse for navigation to collapse -->
-      <span class="navbar-toggler-icon"></span>
-    <!-- class for navbar-->
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-      <!-- ml-auto to push navigation when its full width to right , usually in middle -->
-        <li class="nav-item acive">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="menu.php">Menu</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="aboutus.php">About us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contactus.php">Contact us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="orders.php">Orders</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="adminlogout.php"> logout</a>
-        </li>
-         <li class="nav-item">
-        <a class="nav-link" href="editproducts.php"> Edit Products</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-</head>
-
-
-<body>
+<head>
+	<title>Add/Edit Product</title>
+	<?php include("include/head.inc") ?>
+	<link rel="stylesheet" href="CSS/productedit.css">
 	
-
+</head>
+<body>
+	<?php include("include/nav.inc") ?>
 	<?php
 	if(isset($_POST['action'])){
 		$productid = $_POST['action'];
 		$conn  = db_connect();
-		$sql = "SELECT * FROM products where pid= ".$productid;
+		$sql = "SELECT * FROM products where product_id= ".$productid;
 	 $result = $conn->query($sql);
 	 if ($result->num_rows > 0) {
 		 while($row = $result->fetch_assoc()) {
 				$pname = $row["pname"];
-				$pdescription = $row["description"];
 				$pprice = $row["product_price"];
+				$description = $row["description"];
 				$category = $row["category"];
 				
 		 }
@@ -90,15 +40,15 @@ include_once('include/config.php');
                 	<label for="name">* Product ID:</label>
                 </div>
 				<div class="col-md-4">  
-					<input type="text" id="pid" name="pid" size="30" maxlength="50" value= "<?php echo $product_id ?>"  />
+					<input type="text" id="product_id" name="product_id" size="30" maxlength="50" value= "<?php echo $productid ?>"  />
 				</div>
             </div>
 			<div class="row">
 				<div class="col-md-4">
-                	<label for="name">* Name:</label>
+                	<label for="pname">* Name:</label>
                 </div>
 				<div class="col-md-4">  
-					<input type="text" id="name" name="name" size="30" maxlength="50" value= "<?php echo $pname ?>" required />
+					<input type="text" id="pname" name="pname" size="30" maxlength="50" value= "<?php echo $pname ?>" required />
 				</div>
 				<div class="col-md-4">
 					<span id="name_msg" class="error_msg"></span>
@@ -120,15 +70,15 @@ include_once('include/config.php');
                 	<label for="pprice">* Product Price:</label>
                 </div>
 				<div class="col-md-4"> 
-					<input type="number" id="pprice" name="pprice" step="0.01" value= "<?php echo $product_price ?>" required />
+					<input type="number" id="product_price" name="product_price" step="0.01" value= "<?php echo $pprice ?>" required />
                 </div>
 				<div class="col-md-4">
-					<span id="wrprice_msg" class="error_msg"></span>
+					<span id="product_price_msg" class="error_msg"></span>
                 </div>
             </div>
             <div class="row">
 				<div class="col-md-4">
-                	<label for="numavailable">* Category:</label>
+                	<label for="category">* Category:</label>
 				</div>
 				<div class="col-md-4">
                     <input type="text" id="category" name="category" value= "<?php echo $category ?>" required />
@@ -137,7 +87,7 @@ include_once('include/config.php');
 					<span id="num_msg" class="error_msg"></span>
                 </div>
             </div>
-
+			
 		 <div class="row">
 		 <div class="col-md-1">
 		 </div>
@@ -149,9 +99,9 @@ include_once('include/config.php');
         </form>
 		</div>
 	</div>
-
-
-
+	
+	
+	
   <?php include("include/footer.inc") ?>
 </body>
-</html> 
+</html>
--- editproducts.php
+++ editproducts.php
@@ -1,93 +1,44 @@
 <!DOCTYPE html>
 <?php
-
+//must appear BEFORE the <html> tag
 session_start();
 include_once('include/config.php');
 ?>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Golden Lor</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-  <link href="newstyles.css" rel="stylesheet" type="text/css">
-     
- <nav class="navbar navbar-expand-md navbar-light bg-light bg-light sticky-top">
-      <!--- md for breakpoint for pixel to expanded full site navigation , nav light color and background light sticky to fix navigation-->
-  <div class="conainer-fluid">
-    <!-- container fluid for 100% of screen-->
-    <a class="navbar-brand" href=""><img src="img/logo-full.png" class="img-fluid"></a> 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-      <!--  toggle switch for navigation , collapse for navigation to collapse -->
-      <span class="navbar-toggler-icon"></span>
-    <!-- class for navbar-->
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ml-auto">
-      <!-- ml-auto to push navigation when its full width to right , usually in middle -->
-        <li class="nav-item acive">
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="menu.php">Menu</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="aboutus.php">About us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contactus.php">Contact us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="orders.php">Orders</a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" href="adminlogout.php"> logout</a>
-        </li>
-         <li class="nav-item">
-        <a class="nav-link" href="editproducts.php"> Edit Products</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-
+	<title>Add/Edit Product</title>
+	<?php include("include/head.inc") ?>
+	<link rel="stylesheet" href="CSS/editproducts.css">
 </head>
-
-
 <body>
-	  
-
+	<?php include("include/nav.inc") ?>
 
 	<?php
 	
 	 $conn  = db_connect();
 	 $sql = "SELECT * FROM products";
 	 $result = $conn->query($sql);
+
 if ($result->num_rows > 0) {
     // output data of each row?>
-	<div class="container" ">
+	<div class="container" style="text-align: center;">
 		<div id="border">
-		<div class = "col-md-6">
+		<div class = "row">
 			<a href="addproduct.php" class="btn btn-info" role="button" aria-pressed="true">Add a New Product</a>
 		</div>
-		
+		<div class = "row">
 		<?php
     while($row = $result->fetch_assoc()) {
 		?>
 		
-		 
-				<div class="row" style="padding: 30px;">
-
+		  <div class="card">
+				<div class="col" style="padding: 20px;">
+					  
 					<?php
 						echo "<img class = \"picture\" src=\"".$row["image"]."\">"
 					?>
 				</div>
-
+			
 			<div class = "row">
 				<?php echo "<h3>".$row["pname"]."</h3>" ?>
 			</div>
@@ -105,9 +56,9 @@ if ($result->num_rows > 0) {
 				<form action="editproduct.php" method="post">
 					<?php echo "<button type=\"submit\" class=\"btn btn-primary\" name=\"action\" value=\"".$row["product_id"]."\" /> Edit Product </button>" ?>
 				</form>
-
+				
 			</div></div>
-
+		
 		<?php
 	}?>
 	</div></div></div>
@@ -117,12 +68,10 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 	?>
-
-  
+	
+  <?php include("include/footer.inc") ?>
 </body>
-
 </html>
 <style>
 	img {text-align: center;}
-</style> 
-<?php include("include/footer.inc") ?>
+</style>
